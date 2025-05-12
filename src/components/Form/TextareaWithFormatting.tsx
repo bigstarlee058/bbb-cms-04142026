82f321bc5e8325ae83c1d441cd06d99844a1f7ef
cutebear8888 <185819104+cutebear8888@users.noreply.github.com>
2025-05-12 14:25:31 -0400

    Update the Text editor

--- src/components/Form/TextareaWithFormatting.tsx
+++ src/components/Form/TextareaWithFormatting.tsx
@@ -1,8 +1,9 @@
 import { FormikProps } from 'formik';
-import ReactQuill from 'react-quill';
 import './quill.snow.css';
+import { useQuill} from 'react-quilljs';
 
 import { getFormikError, hasError } from '@/utils/formik';
+import { useEffect } from 'react';
 
 interface Props {
   formik: FormikProps<any>;
@@ -14,33 +15,36 @@ interface Props {
 type InputT = Props & React.InputHTMLAttributes<HTMLInputElement>;
 
 export const TextareaWithFormatting = ({ label, formik, name, placeholder }: InputT) => {
+  const { quill, quillRef } = useQuill({
+    modules: {
+      toolbar: [
+        [{ 'header': [1, 2, 3, 4, false] }],
+        ['bold', 'italic', 'underline', 'strike'],
+        [{'list': 'ordered'}, {'list': 'bullet'}, {'indent': '-1'}, {'indent': '+1'}],
+      ],
+    },
+    formats: ['header', 'bold', 'italic', 'underline', 'strike', 'list', 'bullet', 'indent',],
+  });
+
   const error = getFormikError({ formik, name });
   const isInvalid = hasError({ formik, name });
   const fieldValue = formik.getFieldProps(name).value;
 
   console.log("fieldValue", fieldValue);
 
+  useEffect(() => {
+    if (quill) {
+      quill.clipboard.dangerouslyPasteHTML(fieldValue || '');
+      quill.on('text-change', (delta, oldDelta, source) => {
+        formik.setFieldValue(name, quill.root.innerHTML);
+      });
+    }
+  }, [quill]);
+
   return (
     <div className={`form-group py-2 ${isInvalid ? 'invalid' : 'valid'} h-[250px]`}>
       {label && <label className="fieldLabel">{label}</label>}
-      <ReactQuill
-        theme="snow"
-        placeholder={placeholder}
-        value={fieldValue || ''}
-        onChange={(value) => formik.setFieldValue(name, value)}
-        className='rounded-lg'
-        modules={{
-          toolbar: [
-            // [{ 'header': [1, 2, 3, 4, false] }],
-            ['bold', 'italic', 'underline','strike'],
-            ['clean']
-          ],
-        }}
-        formats={[
-            // 'header',
-            'bold', 'italic', 'underline', 'strike',
-        ]}
-      />
+      <div ref={quillRef} />
       {isInvalid && <p className="text-xs text-danger mt-1">{error}</p>}
     </div>
   );
