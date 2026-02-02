6be772b640881a1a778bc1be83f5f4d8991ecbc7
naderkamelaponar <nader.kamel.the.programmer@gmail.com>
2026-02-02 22:07:04 +0200

    BBB-1023 fix paginations

--- src/features/exercises/Exercises.tsx
+++ src/features/exercises/Exercises.tsx
@@ -1,4 +1,4 @@
-import { useEffect } from 'react';
+import { useEffect ,useState} from 'react';
 import { ContentLayout } from '@/components/Layout';
 import { CreateExercise } from './components/CreateExercise';
 import { ExercisesList } from './components/ExercisesList';
@@ -7,18 +7,45 @@ import { fetchExercises } from './api'
 import { useQuery } from 'react-query';
 import { useUserStore } from '@/stores/user';
 import { LanguageSwitcher, useListTranslations } from '@/components/Language';
-
+import { useFilteringStore } from '@/stores/filter';
+import { Filters } from '@/types';
 export const Exercises = () => {
+  const [currentPage, setCurrentPage] = useState(1);
+  const { search, sortBy } = useFilteringStore();
+  const [filters, setFilters] = useState<Filters>({
+    perPage: 10,
+    page: 1
+  });
+
+  const { setCurrentPage: setStorePage } = useUserStore();
 
   const { data: exerciseTitles } = useQuery('get-exercise-titles', () => fetchExerciseTitles({ filterString: '' }));
-
   const { data: equipmentTitles } = useQuery('get-equipment-titles', () => fetchEquipmentTitles({ filterString: '' }));
-
   const { data: categoryTitles, refetch: refetchCategoryTitles } = useQuery('get-category-titles', () => fetchCategoryTitles({ filterString: '' }));
   const { data: tagTitles, refetch: refetchTagTitles } = useQuery('get-tag-titles', () => fetchTagTitles({ filterString: '' }));
-  const { data: exercises, isLoading } = useQuery(['get-exercises'], () => fetchExercises({}));
-  const { setCurrentPage } = useUserStore();
-  const data = exercises?.exercises||[]
+  
+  const { data: exercises, isLoading } = useQuery(
+    ['get-exercises', filters], 
+    () => fetchExercises(filters),
+    { keepPreviousData: true }
+  );
+
+  useEffect(() => {
+    setFilters((prev) => ({ ...prev, page: currentPage }));
+  }, [currentPage]);
+
+  useEffect(() => {
+    setFilters((prev) => ({ ...prev, search: search, page: 1 }));
+    setCurrentPage(1);
+  }, [search]);
+
+  useEffect(() => {
+    setFilters((prev) => ({ ...prev, sortBy: sortBy?.value, page: 1 }));
+    setCurrentPage(1);
+  }, [sortBy?.value]);
+
+  const data = exercises?.exercises || [];
+
   const {
     selectedLang,
     setSelectedLang,
@@ -26,12 +53,14 @@ export const Exercises = () => {
     hasTranslations,
     getValue,
   } = useListTranslations({
-    data: data ? data : [],
+    data: data,
     translatableFields: ['title', 'description', 'thumbnail', 'videoThumbnail', 'vimeoId'],
   });
+
   useEffect(() => {
-    setCurrentPage('equipments');
+    setStorePage('equipments');
   }, []);
+
   if (isLoading) {
     return (<>Getting the Available Exercises</>)
   }
@@ -57,8 +86,13 @@ export const Exercises = () => {
           onTagCreate={refetchTagTitles}
         />
       </div>
-      <div className="mt-1">
-        <ExercisesList getValue={getValue} />
+            <div className="mt-1">
+        <ExercisesList 
+          getValue={getValue} 
+          exercises={exercises}
+          currentPage={currentPage}
+          setCurrentPage={setCurrentPage}
+        />
       </div>
     </ContentLayout>
   );
